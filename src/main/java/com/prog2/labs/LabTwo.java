2b18e4204ba741b44ac69436b27f47b437170b48
Marie Andrews <marie.andrews2222@gmail.com>
2024-02-05 13:07:38 -0500

    Marie Andrews Lab 2 Completed For Real

--- src/main/java/com/prog2/labs/LabTwo.java
+++ src/main/java/com/prog2/labs/LabTwo.java
@@ -19,22 +19,68 @@ public class LabTwo {
 	
 	public double calcAmountOfShipping(double costOfPurchase) {
 		
+                double shipping = 0;
+                
+                if (costOfPurchase <= 20.00)
+                    shipping = 5.99;
+                else if (costOfPurchase > 20.00 && costOfPurchase <= 65.00)
+                    shipping = 10.99;
+                else if (costOfPurchase > 65.00)
+                    shipping = 15.99;
 		
-		return 0.0;
+		return shipping;
 	}
 	
 	
 	public String findPrimeNumbersUptoInput(int input) {
-		
-		
-		return "";
-	}
+                
+                 int i;
+                 int j;
+                 String results = "";
+                 boolean isPrime = true;
+                 for (i = 2; i <= input; i++) {
+                     
+                     for (j = 2; j < i; j++){
+                         
+                         if (i % j == 0){    // i is not a prime number
+                            isPrime = false;
+                         }
+                         
+                         
+                         }
+                        if (isPrime){
+                            String stringI = Integer.toString(i);
+                            results = results + " " + stringI;
+                        }
+                         isPrime = true;
+                     }
+               
+                    return results.trim();
+             
+        }
+
 	
 	
 	public String generateShape(int numberOfRows) {
 		
-		
-		return "";
+            int line = 1;
+            int number = 1;
+            int lengthOfLine = 1;
+            String results = "";
+            
+            for (line = 1; line <= numberOfRows; line++) {
+                
+                for (lengthOfLine = 1; lengthOfLine <= line; lengthOfLine++) {
+                    String stringI = Integer.toString(number);
+                    results = results + stringI + " ";
+                    number++;
+                }
+                if (line < numberOfRows){
+                    results = results.trim() + "\n";
+                }
+                
+            }
+		return results.trim();
 	}
 	
 	/**
